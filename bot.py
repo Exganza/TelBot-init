f13ca915481a2e3450ecd1371c74975a44504521
ziad aqbawi <exganza@gmail.com>
2022-01-20 12:26:40 +0300

    bot init format

---
+++ bot.py
@@ -0,0 +1,29 @@
+import telebot
+import time
+
+# ضع التوكن هنا
+bot_token = '******************************'
+bot = telebot.TeleBot(token=bot_token)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    pass
+
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    pass
+
+
+@bot.message_handler(func=lambda msg: msg.text is not None)
+def at_bot(message):
+    text = message.text
+    
+    
+    
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
